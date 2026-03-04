e46297da2143c5bfcf944cf73801d2ebcf21a946
PotatoIsHungry <barbora.marie.davidova@gmail.com>
2026-03-04 15:03:45 +0100

    Basic movement
    a/w(jump)/d movement and flipping image

---
+++ Scribbles/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float movementSpeed = 5f;
+    bool isFacingRight = false;
+    float jumpPower = 4f;
+    bool isJumping = false;
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         horizontalInput = 0;
+        if (Keyboard.current != null) 
+        {
+            if (Keyboard.current.aKey.isPressed) 
+                horizontalInput = -1f;
+            if (Keyboard.current.dKey.isPressed) 
+                horizontalInput = 1f;
+        }
+       
+        flipSprite();
+
+        if(Keyboard.current.wKey.isPressed && !isJumping)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            isJumping = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+      rb.linearVelocity = new Vector2(horizontalInput * movementSpeed, rb.linearVelocity.y);
+    }
+
+    void flipSprite()
+    {
+        if((isFacingRight && horizontalInput < 0f) || (!isFacingRight && horizontalInput > 0f))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x = ls.x * -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
