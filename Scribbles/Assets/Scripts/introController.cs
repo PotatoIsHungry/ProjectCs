7753011e8fedeedfd128fcadf83b1b47b87a0e25
PotatoIsHungry <barbora.marie.davidova@gmail.com>
2026-04-16 14:21:24 +0200

    TutorialScene + details in tutorial

---
+++ Scribbles/Assets/Scripts/introController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introController : MonoBehaviour
+{
+     private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        animator=GetComponent<Animator>();
+        StartCoroutine(PlayAndLoad());
+    }
+
+    System.Collections.IEnumerator PlayAndLoad()
+    {
+        animator.Play("intro");
+
+        yield return new WaitForSeconds(
+            animator.GetCurrentAnimatorStateInfo(0).length
+        );
+
+        SceneManager.LoadScene("Menu Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
